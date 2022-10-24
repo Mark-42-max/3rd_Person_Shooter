fab7d513438d537140db0606b24ad50d538262a6
Soham Das <publicsoham.24@gmail.com>
2022-10-24 12:47:15 +0530

    obstacles added, player rotation synced with camera

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,10 +9,13 @@ public class PlayerController : MonoBehaviour
     private CharacterController controller;
     private Vector3 playerVelocity;
     private bool groundedPlayer;
-    private float playerSpeed = 2.0f;
-    private float sprintSpeed = 2.0f;
-    private float jumpHeight = 1.0f;
-    private float gravityValue = -9.81f;
+
+
+    [SerializeField] private float playerSpeed = 2.0f;
+    [SerializeField] private float sprintSpeed = 2.0f;
+    [SerializeField] private float jumpHeight = 1.0f;
+    [SerializeField] private float gravityValue = -9.81f;
+    [SerializeField] private float rotateSpeed = 0.8f;
 
     private PlayerInput playerInput;
 
@@ -20,10 +23,13 @@ public class PlayerController : MonoBehaviour
     private InputAction jumpAction;
     private InputAction sprintAction;
 
+    private Transform cameraTransform;
+
     private void Start()
     {
         controller = GetComponent<CharacterController>();
         playerInput = GetComponent<PlayerInput>();
+        cameraTransform = Camera.main.transform;
 
         moveAction = playerInput.actions["Move"];
         jumpAction = playerInput.actions["Jump"];
@@ -42,13 +48,23 @@ public class PlayerController : MonoBehaviour
         //movement
         Vector2 input = moveAction.ReadValue<Vector2>();
         Vector3 move = new Vector3(input.x, 0, input.y);
+        move = move.x * cameraTransform.right.normalized + move.z * cameraTransform.forward.normalized;
+        move.y = 0;
 
         if(input != Vector2.zero && sprintAction.IsPressed())
         {
+            //rotate
+
+            Quaternion targetRotation = Quaternion.Euler(new Vector3(0, cameraTransform.eulerAngles.y, 0));
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotateSpeed);
             controller.Move(playerSpeed * sprintSpeed * Time.deltaTime * move);
         }
         else if(input != Vector2.zero)
         {
+            //rotate
+
+            Quaternion targetRotation = Quaternion.Euler(new Vector3(0, cameraTransform.eulerAngles.y, 0));
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotateSpeed);
             controller.Move(playerSpeed * Time.deltaTime * move);
         }
 
